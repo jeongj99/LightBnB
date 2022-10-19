5a2b2f62ecd162c87c749b32de2be49d3e9c006b
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-19 17:20:21 +0000

    Create a select query for the property listing by city (vancouver)

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,7 @@
+SELECT properties.id, title, cost_per_night, avg(rating) as average_rating
+FROM properties
+LEFT JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING avg(rating) >= 4
+LIMIT 10;
